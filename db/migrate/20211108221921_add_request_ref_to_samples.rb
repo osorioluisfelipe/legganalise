343ca051be30acd5cc051363fb9e13c0ab961441
paulocarvalho <carvalhofilhopr@gmail.com>
2021-11-10 14:50:50 -0300

    ajuste de rota sample_analysis to request_id

--- db/migrate/20211108221921_add_request_ref_to_samples.rb
+++ db/migrate/20211108221921_add_request_ref_to_samples.rb
@@ -1,5 +1,5 @@
 class AddRequestRefToSamples < ActiveRecord::Migration[6.0]
   def change
-    add_reference :samples, :request, foreign_key: true
+    add_reference :samples, :request, null:false, foreign_key: true
   end
 end
--- db/migrate/20211110135031_change_column_request_id_null_in_samples.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnRequestIdNullInSamples < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :samples, :request_id, false
-  end
-end
