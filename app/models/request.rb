8467dfe508a8b872f3f8bc211535224d8e3a1d8a
Luis Osorio <losorio1986@gmail.com>
2021-11-10 13:43:51 -0300

    ajuste de dependent destroy

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
   belongs_to :user
-  has_many :samples
+  has_many :samples, dependent: :destroy
   has_many :lab_analysis, through: :samples
   validates :project_name, :project_summary, presence: true
 
--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,6 +1,6 @@
 class Sample < ApplicationRecord
   belongs_to :request
-  has_many :sample_analyses
+  has_many :sample_analyses, dependent: :destroy
   has_many :lab_analyses, through: :sample_analyses
 
   validates :sample_name, :sample_type, :sample_matrix, presence: true
