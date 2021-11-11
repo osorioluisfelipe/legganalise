2834fee35f588ac1f463ae5f710f7738557a56db
Luis Osorio <losorio1986@gmail.com>
2021-11-11 19:16:35 -0300

    método e config de admin para tables de active_storage

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,1 +1,9 @@
-config.excluded_models = ['ActiveStorage::Blob', 'ActiveStorage::Attachment']
+RailsAdmin.config do |config|
+  # not real models
+  # https://github.com/sferik/rails_admin/issues/3014
+  config.excluded_models = ['ActiveStorage::Blob', 'ActiveStorage::Attachment']
+
+#   config.authenticate_with do
+#     redirect_to(main_app.root_path, flash: {warning: 'You must be signed-in as an administrator to access that page'}) unless signed_in? && current_user.admin?
+# end
+end
