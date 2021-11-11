f201734302232193e3650610d43ff6495ab831b3
paulocarvalho <carvalhofilhopr@gmail.com>
2021-11-11 18:57:54 -0300

    ajuste no horário UTC

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_185902) do
+ActiveRecord::Schema.define(version: 2021_11_11_214614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "lab_analyses", force: :cascade do |t|
     t.string "analysis_name"
@@ -23,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_185902) do
 
   create_table "requests", force: :cascade do |t|
     t.integer "sample_quantity"
-    t.datetime "request_date", default: "2021-11-09 15:00:40"
+    t.datetime "request_date", default: "2021-11-11 20:11:33"
     t.datetime "approval_date"
     t.datetime "results_date"
     t.string "project_name"
@@ -75,6 +96,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_185902) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "requests", "users"
   add_foreign_key "sample_analyses", "lab_analyses"
   add_foreign_key "sample_analyses", "samples"
