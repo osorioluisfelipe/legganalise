5d298c50828806eb8817d01bce2a9f83804d8ffa
Luis Osorio <losorio1986@gmail.com>
2021-11-11 18:58:02 -0300

    schema atualizado

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,27 @@ ActiveRecord::Schema.define(version: 2021_11_11_185902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "lab_analyses", force: :cascade do |t|
     t.string "analysis_name"
     t.datetime "created_at", precision: 6, null: false
@@ -23,7 +44,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_185902) do
 
   create_table "requests", force: :cascade do |t|
     t.integer "sample_quantity"
-    t.datetime "request_date", default: "2021-11-09 15:00:40"
+    t.datetime "request_date", default: "2021-11-05 18:02:50"
     t.datetime "approval_date"
     t.datetime "results_date"
     t.string "project_name"
@@ -62,19 +83,19 @@ ActiveRecord::Schema.define(version: 2021_11_11_185902) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
-    t.string "organization"
-    t.boolean "admin", default: false
-    t.boolean "tecnician", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
+    t.string "organization"
+    t.boolean "admin", default: false
+    t.boolean "tecnician", default: false
     t.string "last_name"
     t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "requests", "users"
   add_foreign_key "sample_analyses", "lab_analyses"
   add_foreign_key "sample_analyses", "samples"
